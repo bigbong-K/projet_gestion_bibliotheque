46d51950bc4cde360379e44a94e49e5e7b5116e7
tati bongue <tatibongue@gmail.com>
2025-05-17 17:35:40 +0200

    feat<database>: ajout du fichier bibliotheque.sql

---
+++ database/bibliotheque.sql
@@ -0,0 +1,30 @@
+-- On crée la base de données
+CREATE DATABASE bibliotheque;
+
+-- On se connecte à la base de données
+\c bibliotheque
+
+-- On crée la table categorie
+CREATE TABLE IF NOT EXISTS categorie(
+    id_categorie SERIAL PRIMARY KEY,
+    nom VARCHAR(50),
+    lien VARCHAR(100)
+);
+
+-- On crée la table livre
+CREATE TABLE IF NOT EXISTS livre(
+    id_livre SERIAL PRIMARY KEY,
+    id_categorie INT NOT NULL,
+    prix NUMERIC(6,2),
+    disponibilite INT,
+    note INT,
+    code_upc VARCHAR(50),
+    description VARCHAR(300),
+    image_url VARCHAR(100),
+    lien VARCHAR(100),
+
+    FOREIGN KEY (id_categorie)
+    REFERENCES categorie (id_categorie)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+);
