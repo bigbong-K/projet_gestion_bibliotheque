3b0673dfd7a3a126dd860691e36419b97d7782e4
tati bongue <tatibongue@gmail.com>
2025-05-18 09:47:14 +0200

    feat<connection>: ajout du main pour le transfert des données dans la base de données

--- database/connection.py
+++ database/connection.py
@@ -35,3 +35,16 @@ def insert_dataframe(df:pd.DataFrame, nom_table:str):
         cursor.execute(query, tuple(row))
     conn.commit()
     print(f"Données insérée dans la table {nom_table}")
+    
+if __name__ == "__main__":
+    
+    df_categories = pd.read_csv("../data/categories.csv", sep=",")
+    df_livres = pd.read_csv("../data/books.csv", sep=",")
+    
+    # On envoie nos données dans la base de données
+    insert_dataframe(df=df_categories, nom_table="categorie")
+    insert_dataframe(df=df_livres, nom_table="livre")
+    
+    # Ferme la connection
+    cursor.close()
+    conn.close()
